83d84a67c417abaa1e1d4a2b5f04b16d0f233492
fatihaaksoz <fatih.aksoz19@gmail.com>
2021-02-08 09:51:54 +0300

    jsx based

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+
+//Class yazimi 
+
+// class App extends React.Component{
+//     render(){
+//         return <p>Benim Adim ss</p>;
+//     }
+// }
+
+//************************************************* 
+
+//Function yazimi
+
+// function App(){
+//     return <p>Benim Adim </p>;
+// }
+
+//************************************************* 
+
+//JSX
+
+// const names = ["Jale", "Mehmet", "Sinan"];
+
+// function App() {
+//     return (
+//         // JSX ifadeleri tek bir ana elementin icinde olmalidir. 
+
+//         <div>
+//             {names.map(name => <h1>{name}</h1>)}
+
+//             <h2>Fatih</h2>
+
+//         </div>
+
+//         //<h2>Fatih</h2> Yorum satirini kaldirdigimiz anda hata verecektir. Cunku jsx ifadeleri tek bir ana elementte olmalidir.
+//     );
+// }
+
+
+//************************************************* 
+
+// JSX de nasil sytle verilir.
+
+function App() {
+    return (
+        //JSX de elementlere 1 idli buttondaki gibi sytle verilemez. Dogru olan id 2 deki gibidir.
+        <div>
+            
+            {/* <button id="1" type="button" style="padding: 10px; color: white; background-color: red">Button</button> */}
+
+            <button type="button" style={{padding:'10px',color:'white', backgroundColor:'red'}}>Button</button>
+
+        </div>
+    );
+}
+
+ReactDom.render(
+    <App />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
